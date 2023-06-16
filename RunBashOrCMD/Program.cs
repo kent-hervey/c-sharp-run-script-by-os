1e4697fd87aa92383f89de8a7bab51899aa4d01d
Kent Hervey <kent.hervey@zealitconsultants.com>
2023-06-15 22:13:31 -0500

    extracted runShellScript; removed unused code

--- RunBashOrCMD/Program.cs
+++ RunBashOrCMD/Program.cs
@@ -9,51 +9,54 @@ class Program
     {
         Console.WriteLine("Hello, World!");
 
-        string bubba = WhatOS();
-        Console.WriteLine("your operationg system is: " + bubba);
+        string foundOs = WhatOS();
+        Console.WriteLine("your operationg system is: " + foundOs);
 
-        if (bubba.Contains("OSX"))
+        if (foundOs.Contains("OSX"))
         {
             Console.WriteLine("you are on a Mac");
-            Console.WriteLine();
-
-            string scriptPath = Path.Combine("/Users/khervey/VisualStudioProjects/RunBashOrCMD", "utilities", "createVersionTxt.sh");
-
-
-
-            Console.WriteLine("here is the file path " + scriptPath);
-
-            // Create a new ProcessStartInfo object
-            ProcessStartInfo startInfo = new ProcessStartInfo
-            {
-                FileName = "/bin/bash",
-                Arguments = scriptPath,
-                UseShellExecute = false,
-                RedirectStandardOutput = true
-            };
-
-            // Start the process
-            Process process = new Process
-            {
-                StartInfo = startInfo
-
-            };
-
-            process.Start();
-
-            // Wait for the process to finish
-            process.WaitForExit();
-
-            // Get the output from the process
-            string output = process.StandardOutput.ReadToEnd();
-
-            // Print the output
-            Console.WriteLine(output);
-
+            String version = runShellScript();
         }
 
         Console.ReadKey();
     }
+
+    private static string runShellScript()
+    {
+        string scriptPath = Path.Combine("/Users/khervey/VisualStudioProjects/RunBashOrCMD", "utilities", "createVersionTxt.sh");
+
+        Console.WriteLine("here is the file path " + scriptPath);
+
+        // Create a new ProcessStartInfo object
+        ProcessStartInfo startInfo = new ProcessStartInfo
+        {
+            FileName = "/bin/bash",
+            Arguments = scriptPath,
+            UseShellExecute = false,
+            RedirectStandardOutput = true
+        };
+
+        // Start the process
+        Process process = new Process
+        {
+            StartInfo = startInfo
+
+        };
+
+        process.Start();
+
+        // Wait for the process to finish
+        process.WaitForExit();
+
+        // Get the output from the process
+        string output = process.StandardOutput.ReadToEnd();
+
+        // Print the output
+        Console.WriteLine(output);
+
+        return output;
+    }
+
 
     public static string WhatOS()
     {
